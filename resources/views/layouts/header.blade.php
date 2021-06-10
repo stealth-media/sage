623e3124e2e1584e51b5600bc5e3799da0909b8d
brent-stealth <brent@stealthmedia.com>
2021-06-10 14:35:01 -0600

    fix fontawesome

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -9,6 +9,8 @@
 
     <meta name="theme-color" content="#141414"/>
 
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
+
     @yield('styles')
 
     @stack('header.scripts')
