6d9642e9fbc4b8894f77b60b635f54cce097dc5c
brent-stealth <brent@stealthmedia.com>
2021-06-24 15:29:16 -0600

    Add Dan's scss

--- functions.php
+++ functions.php
@@ -47,3 +47,36 @@
     function the_image($image, $class = '', $size = 'full') {
         echo wp_get_attachment_image($image['ID'], $size, '', array('alt' => $image['alt'], 'class' => $class));
     }
+
+    /**
+     * Add numeric pagination
+     * @param int $page_count
+     * @param null $query
+     */
+    function numeric_pagination( $page_count = 9, $query = null ) {
+        if ( null == $query ) {
+            global $wp_query;
+            $query = $wp_query;
+        }
+
+        if ( 1 >= $query->max_num_pages ) {
+            return;
+        }
+
+        $big = 9999999999; // need an unlikely integer
+
+        echo '<div class="component--pagination">';
+        echo paginate_links( array(
+            'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+            'format' => '?paged=%#%',
+            'current' => max( 1, get_query_var( 'paged' ) ),
+            'total' => $query->max_num_pages,
+            'end_size' => 0,
+            'type' => 'list',
+            'prev_next' => false,
+            'mid_size' => $page_count,
+            'next_text' => __( '', 'textdomain' ),
+            'prev_text' => __( '', 'textdomain' )
+        ) );
+        echo '</div>';
+    }
